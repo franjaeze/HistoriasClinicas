b6ada287c64d8a415f5a9985cec5085324cf819b
paoquinonez <paola.quinonez87@gmail.com>
2022-08-31 23:18:56 -0300

    Clases Base Creadas

--- Historias Clinicas/obj/Release/netcoreapp3.1/Historias Clinicas.RazorAssemblyInfo.cs
+++ Historias Clinicas/obj/Release/netcoreapp3.1/Historias Clinicas.RazorAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Historias Clinicas")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Historias Clinicas")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Historias Clinicas")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.RelatedAssemblyAttribute("Historias Clinicas.Views")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
