4c013a6b3e9e9a9b3792fd63555da0e8a675e9fe
fjeveron <fjeveron@gmail.com>
2022-09-12 17:50:28 -0300

    Agrego campos Requierd, DataType y agrego herencia tambien a la clase Usuario

--- Historias Clinicas/Models/Persona.cs
+++ Historias Clinicas/Models/Persona.cs
@@ -1,22 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
+using System.Text;
+using Historias_Clinicas.Models;
 
 namespace Historias_Clinicas.Models
 {
     public class Persona
     {
         public int Id { get; set; }
+
+        [ Required (ErrorMessage = MensajeError.Requerido)]
         public String Nombre { get; set; }
 
+        [Required(ErrorMessage = MensajeError.Requerido)]
+        [DataType(DataType.EmailAddress)]
         public String Email { get; set; }
 
+        [Required(ErrorMessage = MensajeError.Requerido)]
+        [StringLength(50, MinimumLength =2, ErrorMessage = MensajeError.MinMaxString)]
         public String Apellido { get; set; }
 
+        [Required(ErrorMessage = MensajeError.Requerido)]
         public String Dni { get; set; }
 
+        [DataType(DataType.PhoneNumber)]
         public String Telefono { get; set; }
+
 
         public DateTime FechaDeAlta { get; set; }
 
--- Historias Clinicas/Models/Usuario.cs
+++ Historias Clinicas/Models/Usuario.cs
@@ -5,15 +5,10 @@ using System.Threading.Tasks;
 
 namespace Historias_Clinicas.Models
 {
-    public class Usuario
+    public class Usuario : Persona
     {
 
-        public int Id { get; set; }
-        public String Nombre { get; set; }
-        public String Apellido { get; set; }
-
-        public String Email { get; set; }
-
+    
         public DateTime FechaAlta { get; set; }
 
         public String Password { get; set; }
