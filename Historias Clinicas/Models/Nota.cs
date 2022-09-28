0f3f9be1412e0c06420606718caa3421a92777c7
mateobellomo <mateo.bellomo@gmail.com>
2022-09-27 22:50:12 -0300

    Resolviendo Feedback del "05.Planificación"

--- Historias Clinicas/Models/Nota.cs
+++ Historias Clinicas/Models/Nota.cs
@@ -5,12 +5,15 @@ namespace Historias_Clinicas.Models
 {
     public class Nota
     {
-        [Key]
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = MensajeError.Requerido)] // Es un requerimiento completar este campo
+        [StringLength(6, MinimumLength = 1, ErrorMessage = MensajeError.MinMaxString)] // Puede haber entre 0 y 999.999 medicos
+        [RegularExpression(@" ^ [0-9] ", ErrorMessage = MensajeError.NumerosPositivos)]  // En cada caracter solo se puede poner numeros de 0 a 9
         public string MedicoID { get; set; }
 
+        [Required(ErrorMessage = MensajeError.Requerido)] // Es un requerimiento completar este campo
+        [StringLength(10000, MinimumLength = 5, ErrorMessage = MensajeError.MinMaxString)] //Minimo 5 caracteres con maximo 10000
         public string Mensaje { get; set; }
 
         public DateAndTime FechaYHora { get; set; }
