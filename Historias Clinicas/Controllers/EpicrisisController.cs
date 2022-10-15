a9b3ffdc5b14d21beb41573aab566433f7a3b088
fjeveron <fjeveron@gmail.com>
2022-10-15 19:56:11 -0300

    Agrego controllers de clases Epicrisis , Diagnostico y Pacientes

---
+++ Historias Clinicas/Controllers/EpicrisisController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Historias_Clinicas.Data;
+using Historias_Clinicas.Models;
+
+namespace Historias_Clinicas.Controllers
+{
+    public class EpicrisisController : Controller
+    {
+        private readonly HistoriasClinicasContext _context;
+
+        public EpicrisisController(HistoriasClinicasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Epicrisis
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Epicrisis.ToListAsync());
+        }
+
+        // GET: Epicrisis/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var epicrisis = await _context.Epicrisis
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (epicrisis == null)
+            {
+                return NotFound();
+            }
+
+            return View(epicrisis);
+        }
+
+        // GET: Epicrisis/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Epicrisis/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,MedicoId,PacienteId,Resumen,DiasInternacion,FechaYHora,FechaYHoraAlta,FechaYHoraIngreso")] Epicrisis epicrisis)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(epicrisis);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(epicrisis);
+        }
+
+        // GET: Epicrisis/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var epicrisis = await _context.Epicrisis.FindAsync(id);
+            if (epicrisis == null)
+            {
+                return NotFound();
+            }
+            return View(epicrisis);
+        }
+
+        // POST: Epicrisis/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,MedicoId,PacienteId,Resumen,DiasInternacion,FechaYHora,FechaYHoraAlta,FechaYHoraIngreso")] Epicrisis epicrisis)
+        {
+            if (id != epicrisis.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(epicrisis);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EpicrisisExists(epicrisis.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(epicrisis);
+        }
+
+        // GET: Epicrisis/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var epicrisis = await _context.Epicrisis
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (epicrisis == null)
+            {
+                return NotFound();
+            }
+
+            return View(epicrisis);
+        }
+
+        // POST: Epicrisis/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var epicrisis = await _context.Epicrisis.FindAsync(id);
+            _context.Epicrisis.Remove(epicrisis);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EpicrisisExists(int id)
+        {
+            return _context.Epicrisis.Any(e => e.Id == id);
+        }
+    }
+}
