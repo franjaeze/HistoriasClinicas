4a7b3454c9b9fd5616815b927e2f55c4fa246c61
mateobellomo <mateo.bellomo@gmail.com>
2022-10-14 23:37:46 -0300

    Agregado de Controllers

---
+++ Historias Clinicas/Controllers/EvolucionsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Historias_Clinicas.Data;
+using Historias_Clinicas.Models;
+
+namespace Historias_Clinicas.Controllers
+{
+    public class EvolucionsController : Controller
+    {
+        private readonly HistoriasClinicasContext _context;
+
+        public EvolucionsController(HistoriasClinicasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Evolucions
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Evoluciones.ToListAsync());
+        }
+
+        // GET: Evolucions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var evolucion = await _context.Evoluciones
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (evolucion == null)
+            {
+                return NotFound();
+            }
+
+            return View(evolucion);
+        }
+
+        // GET: Evolucions/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Evolucions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,MedicoId,FechaYHoraInicio,FechaYHoraAlta,FechaYHoraCierre,EstadoAbierto,DescripcionAtencion,Indicaciones,PrecisaEstudiosAdicionales,PrecisaInterconsultaMedica")] Evolucion evolucion)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(evolucion);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(evolucion);
+        }
+
+        // GET: Evolucions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var evolucion = await _context.Evoluciones.FindAsync(id);
+            if (evolucion == null)
+            {
+                return NotFound();
+            }
+            return View(evolucion);
+        }
+
+        // POST: Evolucions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,MedicoId,FechaYHoraInicio,FechaYHoraAlta,FechaYHoraCierre,EstadoAbierto,DescripcionAtencion,Indicaciones,PrecisaEstudiosAdicionales,PrecisaInterconsultaMedica")] Evolucion evolucion)
+        {
+            if (id != evolucion.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(evolucion);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EvolucionExists(evolucion.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(evolucion);
+        }
+
+        // GET: Evolucions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var evolucion = await _context.Evoluciones
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (evolucion == null)
+            {
+                return NotFound();
+            }
+
+            return View(evolucion);
+        }
+
+        // POST: Evolucions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var evolucion = await _context.Evoluciones.FindAsync(id);
+            _context.Evoluciones.Remove(evolucion);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EvolucionExists(int id)
+        {
+            return _context.Evoluciones.Any(e => e.Id == id);
+        }
+    }
+}
