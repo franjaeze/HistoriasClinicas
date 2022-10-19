0e1e0deedc47831560c1433c52563ecfa6070030
paoquinonez <paola.quinonez87@gmail.com>
2022-10-19 16:51:25 -0300

    Add Alias

--- Historias Clinicas/Models/Usuario.cs
+++ Historias Clinicas/Models/Usuario.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
-
+using Historias_Clinicas.Helpers;
 
 namespace Historias_Clinicas.Models
 {
@@ -14,6 +14,7 @@ namespace Historias_Clinicas.Models
 
         [Required(ErrorMessage = MensajeError.Requerido)]
         [StringLength(20, MinimumLength = 6, ErrorMessage = MensajeError.MinMaxString)]
+        [Display(Name = Alias.NombreUsuario)]
         public string NombreUsuario { get; set; }    
 
        [Required(ErrorMessage = MensajeError.Requerido)]
