21b8e8634e621bb88ba95fc0a9092e0d4efba48e
paoquinonez <paola.quinonez87@gmail.com>
2022-11-11 19:20:50 -0300

    configuracion Pass generica

--- Historias Clinicas/Helpers/Configs.cs
+++ Historias Clinicas/Helpers/Configs.cs
@@ -7,7 +7,7 @@ namespace Historias_Clinicas.Helpers
 {
     public class Configs
     {
-        public const string PasswordGenerica = "Password1!";
+        public const string PasswordGenerica = "Password1";
         public const string EmpleadoRolName = "Empleado";
         public const string MedicoRolName = "Medico";
         public const string PacienteRolName = "Paciente";
