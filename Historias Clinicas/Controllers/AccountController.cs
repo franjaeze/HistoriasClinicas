bd992616d47d7d462e5abf8317a3a722d23768e6
camilaszesko <camilaszesko@hotmail.com>
2022-11-16 18:44:11 -0300

    solucion error

--- Historias Clinicas/Controllers/AccountController.cs
+++ Historias Clinicas/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using Historias_Clinicas.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Threading.Tasks;
 
